914d402ea1645ccab27e0f97467add529639e682
Marcos Hack <marcoshack@gmail.com>
2013-08-15 19:50:48 -0300

    project creation based on json_spec gem

---
+++ lib/restful/matchers/have_restful_json_link.rb
@@ -0,0 +1,48 @@
+require 'json'
+
+module RESTful
+  module Matchers
+    class HaveRestfulJsonLink
+
+      def initialize(rel, href)
+        @rel  = rel
+        @href = href
+      end
+
+      def matches?(json_string)
+        if links = parse_links(json_string)
+          return links[@rel] == @href
+        else
+          raise StandardError.new("JSON has no RESTful links")
+        end
+      end
+
+      def failure_message_for_should
+        "Expected RESTful link: #{link_representation}"
+      end
+
+      def failure_message_for_should_not
+        "Expected no RESTful link: #{link_representation}"
+      end
+
+      def description
+        "have RESTful link: #{link_representation}"
+      end
+
+      private
+      def link_representation
+        "{ rel: #{@rel}, href: #{@href} }"
+      end
+
+      def parse_links(json_string)
+        json = JSON.parse(json_string)
+        links = nil
+        if json["links"]
+          links = {}
+          json["links"].each { |link| links[link["rel"]] = link["href"] }
+        end
+        return links
+      end
+    end
+  end
+end
---
+++ rspec-restful-matchers.gemspec
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+Gem::Specification.new do |gem|
+  gem.name    = "rspec-restful-matchers"
+  gem.version = "0.1"
+
+  gem.authors     = [ "Marcos Hack" ]
+  gem.email       = [ "marcoshack@gmail.com" ]
+  gem.summary     = "Easily handle RESTful resources in RSpec scenarios"
+  gem.description = "RSpec matchers for testing RESTful resource representations"
+  gem.homepage    = "https://github.com/marcoshack/rspec-restful-matchers"
+  gem.license     = "MIT"
+
+  gem.add_dependency "rspec", "~> 2.0"
+
+  gem.add_development_dependency "bundler", "~> 1.0"
+
+  gem.files         = `git ls-files`.split($\)
+  gem.test_files    = gem.files.grep(/^(test)/)
+  gem.require_paths = ["lib"]
+end
